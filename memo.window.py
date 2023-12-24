17b5ff8f8ec681159d4f8a05d3c9978525da389d
vladislavich245 <150582843+vladislavich245@users.noreply.github.com>
2023-12-24 12:32:53 +0300

    Update memo.window.py

--- memo.window.py
+++ memo.window.py
@@ -1,66 +1,39 @@
-from memo_card_layout import (
-    app, layout_card,
-    lb_Question, lb_Correct, lb_Result,
-    rbtn_1, rbtn_2, rbtn_3, rbtn_4,
-    btn_OK, show_question, show_result
-)
-from PyQt5.QtWidgets import QWidget, QApplication
-from random import shuffle
+import PyQt5.QtWidgets import QApplscation, QWidget, QlineEdit,\
+        QHBoxLayout, QVBoxLayout, QPushButton, QLabel
 
+menu_win= QWidget()
 
-card_width, card_height = 600, 500
-text_wrong = 'Неправильно'
-text_correct = 'Правильно'
+lb_quest = QLabel('Введіть запитання:')
+lb_right_ans = QLabel('Введіть вірну відповідь:')
+lb_wrong_ans1 = QLabel('Введіть першу хибну відповідь')
+lb_wrong_ans2 = QLabel('Введіть другу хибну відповідь')
+lb_wrong_ans3 = QLabel('Введіть третю хибну відповідь')
 
+lb_question = QlineEdit()
+lb_right_ans = QlineEdit()
+lb_wrong_ans1 = QlineEdit()
+lb_wrong_ans2 = QlineEdit()
+lb_wrong_ans3 = QlineEdit()
 
+lb_header_stat = QLabel("Статистика:")
+lb_header_stat.setStyleSheet('font-size: 19px; font-weight; bold;')
 
+lb_statistic = QLabel()
 
-frm_question='Яблуко'
-frm_right='apple'
-frm_wrong1='application'
-frm_wrong2='building'
-frm_wrong3='caterpillar'
+vl_Labels = QVBoxLayout()
+vl_Labels.addWidgets(lb_quest)
+vl_Labels.addWidgets(lb_right_ans)
+vl_Labels.addWidgets(lb_wrong_ans1)
+vl_Labels.addWidgets(lb_wrong_ans2)
+vl_Labels.addWidgets(lb_wrong_ans3)
 
+vl_LineEdits = QVBoxLayout()
+vl_LineEdits.addWidgets(lb_quest)
+vl_LineEdits.addWidgets(lb_right_ans)
+vl_LineEdits.addWidgets(lb_wrong_ans1)
+vl_LineEdits.addWidgets(lb_wrong_ans2)
+vl_LineEdits.addWidgets(lb_wrong_ans3)
 
-radio_list = [rbtn_1,rbtn_2,rbtn_3,rbtn_4]
-shuffle(radio_list)
-answer = radio_list[0]
-wrong_answer1 , wrong_answer2, wrong_answer3=radio_list[1],radio_list[2],radio_list[3]
-
-def show_data:
-    lb_Question.setText(frm_question)
-    lb_Correct.setText(frm_right)
-    answer.setText(frm_right)
-    wrong_answer1.setText(frm_wrong1)
-    wrong_answer2.setText(frm_wrong2)
-    wrong_answer3.setText(frm_wrong3)
-
-
-def check_result():
-    correct = answer.isChecked()
-    if correct:
-        lb_Result.setText(text_correct)
-        show_result()
-    else:
-        incorrect = wrong_answer1.isCheked() or wrong_answer2.isCheked() or wrong_answer3.isCheked()
-        if incorrect:
-            lb_Result.setText(text_wrong)
-            show_result()
-
-
-def clickOK():
-    if bin_OK.text() != 'Наступне питання':
-        check_result()
-win_card = QWidget()
-win_card.resize(card_width,card_height)
-win_card.move(300,300)
-win_card.setWindowTitle('Memory Card')
-
-win_card.setWindowTitle(layout_card)
-show_data()
-show_question()
-bin_OK.clicked.connect(click_OK)
-
-
-win_card.show()
-app.exec_()
+h1_question = QHBoxLayout()
+h1_question.addLayout(vl_Labels)
+h1_question.addLayout(vl_LineEdits)
